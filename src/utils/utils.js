0084fdbf4f9bb2a063f88d499f53e605046582dc
votrongluan <trongluan115@gmail.com>
2025-02-05 16:05:32 +0700

    feat: sample template

---
+++ src/utils/utils.js
@@ -0,0 +1,48 @@
+import dayjs from "dayjs";
+import utc from "dayjs/plugin/utc";
+import timezone from "dayjs/plugin/timezone";
+
+dayjs.extend(utc);
+dayjs.extend(timezone);
+
+export const convertDateStringToDate = (dateString) => {
+  const [day, month, year] = dateString.split("-");
+  const date = new Date(Date.UTC(year, month - 1, day, 0, 0, 0));
+  date.setUTCHours(date.getUTCHours() + 7);
+  return date.toISOString();
+};
+
+export const convertTimeStampToDateString = (timestamp) => {
+  const date = dayjs(timestamp).tz("Asia/Ho_Chi_Minh");
+
+  const dateString = date.format("DD-MM-YYYY");
+
+  return dateString;
+};
+
+export const getDateNow = () => {
+  const currentDate = new Date();
+  const vietnamTime = new Date(
+    currentDate.getTime() + 7 * 60 * 60 * 1000
+  ).toISOString();
+  return vietnamTime;
+};
+
+export const generateBarcode = (type) => {
+  const randomNum = Math.floor(100000 + Math.random() * 900000);
+  return type.typeName.toUpperCase() + randomNum;
+};
+export const convertDateToDateString = (dateString) => {
+  const date = new Date(dateString);
+  const day = date.getUTCDate().toString().padStart(2, "0");
+  const month = (date.getUTCMonth() + 1).toString().padStart(2, "0");
+  const year = date.getUTCFullYear();
+  return `${day}/${month}/${year}`;
+};
+
+export const formatPrice = (price) => {
+  return new Intl.NumberFormat("vi-VN", {
+    style: "currency",
+    currency: "VND",
+  }).format(price);
+};
